b714ba90df934ca7d11df8935c7778356aedba41
archwrth <archwrth@gmail.com>
2026-05-31 14:32:35 +0800

    Fixes

--- build.py
+++ build.py
@@ -5109,7 +5109,7 @@ function renderLineGraph() {
     extraAxes[yKey] = {
       overlaying: 'y', side: 'right',
       anchor: idx === 0 ? 'x' : 'free',
-      position, range, rangemode: 'tozero',
+      position, range,
       showgrid: false, showline: true, linewidth: 1.5,
       linecolor: axColor, tickcolor: axColor,
       tickfont: {size: 8, color: axColor},
@@ -5131,11 +5131,7 @@ function renderLineGraph() {
     autosize:true, font:{family:'Ubuntu, sans-serif'}, margin:{l:sm?45:65, r:sm?8:(hasWeather?50:20), t:state.showSeasonLines?(sm?70:85):(sm?6:10), b:sm?40:60},
     xaxis:{title:t('dateTime') + ' <i><span style="color:#aaa">(EAT, UTC+03:00)</span></i>', type:'date', showgrid:true, gridcolor:'#eee',
       domain: [0, xRight],
-      // 'all time' mode: let Plotly autorange so uirevision can preserve drag-zoom.
-      // Explicit range = developer-managed, which makes Plotly ignore user zoom even with uirevision.
-      ...(state.timeMode === 'all'
-        ? {autorange: true}
-        : {range: [toEATString(start), toEATString(end)]}),
+      range: [toEATString(start), toEATString(end)],
       nticks:20, tickangle:-30, automargin:true},
     yaxis:{title:yTitle, ticksuffix:ySuffix, showgrid:true, gridcolor:'#eee', range: yLo !== undefined ? [yLo, yHi] : (hasWeather ? [0, 1] : undefined)},
     ...extraAxes,
